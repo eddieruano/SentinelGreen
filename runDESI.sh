4ebe73226b5f62101d2f1f8b15ec901c22f3a8d2
Eddie Ruano <eddieruano@gmail.com>
2017-06-16 13:39:42 -0700

    Script

--- runDESI.sh
+++ runDESI.sh
@@ -3,6 +3,8 @@ echo "Starting" > Logs/RunDESI.txt
 cd ~/Desktop/SentinelGreen
 git pull
 echo "Pulling" > Logs/RunDESI.txt
+echo "Starting the Maid" > Logs/RunDESI.txt
+sudo python3 StartMaid.py
 echo "Starting Infinite Loop" > Logs/RunDESI.txt
 while [ true ]; do
  [ -f on.dat ] || sudo python3 MissionControl.py
