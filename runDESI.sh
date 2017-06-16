7f99e96aff74ec477bba8183a1c22f16e2020245
Eddie Ruano <eddieruano@gmail.com>
2017-06-16 13:37:01 -0700

    Script

--- runDESI.sh
+++ runDESI.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-echo "Starting"
+echo "Starting" > Logs/RunDESI.txt
 cd ~/Desktop/SentinelGreen
 git pull
-echo "Pulling"
-[ -f on.dat ] || sudo python3 MissionControl.py
+echo "Pulling" > Logs/RunDESI.txt
+echo "Starting Infinite Loop" > Logs/RunDESI.txt
+while [ true ]; do
+ [ -f on.dat ] || sudo python3 MissionControl.py
+ sleep 40
+done
+
